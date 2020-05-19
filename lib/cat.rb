51900225696531a91a9e03c1f42bbded21782279
kiku ishimoto <kikuish@gmail.com>
2020-05-19 17:31:17 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   attr_reader :name
   attr_accessor :mood
   # code goes here
-  def initialize(name, Owner)
+  def initialize(name, owner)
     @name = name
     @mood = nervous
     @owner = Owner
