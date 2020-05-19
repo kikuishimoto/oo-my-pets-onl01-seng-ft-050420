750c81debd3a810a9af229fe137dd76cbfafb2c5
kiku ishimoto <kikuish@gmail.com>
2020-05-19 17:41:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,11 @@
 class Owner
   attr_accessor :name
+  @@all = []
   def initialize(name)
     @name = name
+  end
+  def cats
+    Cats.all.select {|cat| cat.name == self}
   end
   # code goes here
 end
