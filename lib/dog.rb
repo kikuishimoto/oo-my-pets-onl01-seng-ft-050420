346452c4c556c552eebbc0870180bdb8f91c287c
kiku ishimoto <kikuish@gmail.com>
2020-05-19 17:39:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
 class Dog
   attr_reader :name
   attr_accessor :mood
+  @@all = []
   # code goes here
   def initialize(name, owner)
     @name = name
-    @mood = nervous
+    @mood = 'nervous'
     @owner = Owner
+    @@all << self
   end
+  
 end
